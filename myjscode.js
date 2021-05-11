0c41ed58c9daffa077f76d661489e76b47f03958
Ahmed Shabbir <ahmed.shabbir1308@gmail.com>
2021-05-11 19:11:56 +0500

    API with Jason

--- myjscode.js
+++ myjscode.js
@@ -1,9 +1,70 @@
-$(function () {
-  $("#myForm").submit(handleFormSubmit);
-});
+document.querySelector('.btn-customer').addEventListener('click', getCustomer)
 
-function handleFormSubmit(e) {
-  alert("Form is being submitted");
+document.querySelector('.btn-customers').addEventListener('click', getCustomers)
 
-  e.preventDefault();
+
+// Get a Customer
+function getCustomer(){
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customer.json', true)
+
+  xhr.onload = function(){
+    if(this.status == 200){
+      const customer = JSON.parse(this.responseText)
+
+      const output = `
+        <ul>
+          <li>ID: ${customer.id}</li>
+          <li>Name: ${customer.name}</li>
+          <li>Phone: ${customer.phone}</li>
+          <li>University: ${customer.university}</li>
+        </ul>
+      `
+      document.querySelector('.output1').innerHTML = output
+    }
+  }
+
+  xhr.onerror = function() {
+    document.querySelector('.output1').innerHTML = 
+      `<p>Request Error</p>`
+  }
+
+  xhr.send()
+}
+
+
+// Get Customers
+function getCustomers(){
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', 'customers.json', true)
+
+  xhr.onload = function(){
+    if(this.status == 200){
+      const customers = JSON.parse(this.responseText)
+
+      let output = ''
+
+      customers.forEach(function(users){
+        output += `
+        <ul>
+          <li>ID: ${users.id}</li>
+          <li>Name: ${users.name}</li>
+          <li>Phone: ${users.phone}</li>
+          <li>University: ${users.university}</li>
+        </ul>
+      `
+      });
+      
+      document.querySelector('.output2').innerHTML = output
+    }
+  }
+
+  xhr.onerror = function() {
+    document.querySelector('.output2').innerHTML = 
+      `<p>Request Error</p>`
+  }
+
+  xhr.send()
 }
