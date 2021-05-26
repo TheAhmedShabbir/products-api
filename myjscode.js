da748aca5df75f430cc9b1719aec37155c91436b
oyeahmed <ahmed.shabbir1308@gmail.com>
2021-05-26 23:07:11 +0500

    Arrow functions

--- myjscode.js
+++ myjscode.js
@@ -7,53 +7,41 @@ document.querySelector('.btn3').addEventListener('click', getAPI)
 // Get text
 function getText(){
   fetch('output.txt')
-    .then(function(res){
-      return res.text()
-  })
-    .then(function(data){
-      document.querySelector('.output').innerHTML = data
-    })
-    .catch(function(err){
-      console.log(err)
-    })
+    .then(res => res.text())
+
+    .then(data => document.querySelector('.output').innerHTML = data)
+    
+    .catch(err => console.log(err))
 }
 
 // Get JSON Data
 function getJSON(){
   fetch('posts.json')
-    .then(function(res){
-      return res.json()
-  })
-    .then(function(data){
+    .then(res => res.json())
+
+    .then(data => {
       let output = ''
 
-      data.forEach(function(post) {
-        output += `<li>${post.title}</li>`
-      });
+      data.forEach(post => output += `<li>${post.title}</li>`)
 
       document.querySelector('.output').innerHTML = output
     })
-    .catch(function(err){
-      console.log(err)
-    })
+
+    .catch(err => console.log(err))
 }
 
 // Get API Data
 function getAPI(){
   fetch('https://usman-recipes.herokuapp.com/api/products')
-    .then(function(res){
-      return res.json()
-  })
-    .then(function(data){
+    .then(res => res.json())
+
+    .then(data => {
       let output = ''
 
-      data.forEach(function(products) {
-        output += `<li>${products.name}</li>`
-      });
+      data.forEach(products => output += `<li>${products.name}</li>`)
 
       document.querySelector('.output').innerHTML = output
     })
-    .catch(function(err){
-      console.log(err)
-    })
+    
+  .catch(err => console.log(err))
 }
